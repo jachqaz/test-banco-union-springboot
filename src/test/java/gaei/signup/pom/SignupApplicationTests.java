e8d1ea1dde3c4e63c70fe4250815789a6be0820f
Johan Castaño <jach_1997@hotmail.com>
2024-10-08 16:55:36 -0500

    Commit initial
    Signed-off-by: Johan Castaño <jach_1997@hotmail.com>

---
+++ src/test/java/gaei/signup/pom/SignupApplicationTests.java
@@ -0,0 +1,13 @@
+package gaei.signup.pom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
